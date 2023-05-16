95e1794e75812b7fab42967e117a8f2f8a811110
SapryWenInera <pedro_americo@id.uff.br>
2023-05-16 16:02:06 -0300

    Files for image build

---
+++ Containerfile
@@ -0,0 +1,20 @@
+ARG IMAGE_NAME="${IMAGE_NAME:-silverblue}"
+ARG SOURCE_IMAGE="$SOURCE_IMAGE:-silverblue}"
+ARG BASE_IMAGE="quay.io/fedora-ostree-desktops/${SOURCE_IMAGE}"
+ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-38}"
+
+FROM ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION} AS builder
+
+ARG IMAGE_NAME="${IMAGE_NAME}"
+ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION}"
+
+ADD build.sh /tmp/build.sh
+ADD packages.json /tmp/packages.json
+ADD repos.json /tmp/repos.json
+
+COPY --from=ghrc.io/sapryweninera/config:latest /rpms /tmp/rpms
+
+RUN /tmp/build.sh
+RUN rm -rf /tmp/* /var/*
+RUN ostree container commit
+RUN mkdir -p /var/tmp && chmod -R 177 /var/tmp
---
+++ build.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+EXTRA_REPOS=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+	(select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+	| sort | unique[]" /tmp/repos.json))
+
+REMOVED_REPOS=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+	(select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+	| sort | unique[]" /tmp/repos.json))
+
+INCLUDED_PACKAGES=($(jq -r "[(all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+	(select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+	| sort | unique[]" /tmp/packages.json))
+
+EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+                             | sort | unique[]" /tmp/packages.json))
+
+
+if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
+fi
+
+echo "-- Adding RPM-FUSION RPMS --"
+wget -P /tmp/rpms \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+
+echo "---"
+
+echo "-- Repositories Management --"
+
+if [[ "${#EXTRA_REPOS[@]}" -gt 0 && "${#REMOVED_REPOS[@]}" -eq 0 ]]; then
+    wget -P /etc/yum.repos.d/ \
+        ${EXTRA_REPOS[@]}
+
+elif [[ "${#EXTRA_REPOS[@]}" -eq 0 && "${#REMOVED_REPOS[@]}" -gt 0 ]]; then
+    rm -f /etc/yum.repos.d/\
+        ${REMOVED_REPOS[@]}
+
+elif [[ "${#EXTRA_REPOS[@]}" -gt 0 && "${#REMOVED_REPOS[@]}" -gt 0 ]]; then
+    rm -f /etc/yum.repos.d\
+        ${REMOVED_REPOS[@]} && \
+        wget -P /etc/yum.repos.d \
+	${EXTRA_REPOS[@]}
+
+else
+    echo "No repos to add."
+
+fi
+
+echo "---"
+
+echo "Installing RPM-FUSION RPMS"
+rpm-ostree install \
+    /tmp/rpms/*.rpm \
+    fedora-repos-archive
+
+echo "---"
+
+echo " -- Packages Management --"
+
+if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -eq 0 ]]; then
+    rpm-ostree install \
+        ${INCLUDED_PACKAGES[@]}
+
+elif [[ "${#INCLUDED_PACKAGES[@]}" -eq 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    rpm-ostree override remove \
+        ${EXCLUDED_PACKAGES[@]}
+
+elif [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    rpm-ostree override remove \
+        ${EXCLUDED_PACKAGES[@]} \
+        $(printf -- "--install=%s " ${INCLUDED_PACKAGES[@]})
+
+else
+    echo "No packages to install."
+
+fi
+
+echo "---"
