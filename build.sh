adc9f594ba5e8f148274ba3c10ca48a04bd30a64
SapryWenInera <pedro_americo@id.uff.br>
2023-05-17 15:46:42 -0300

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ RELEASE="$(rpm -E %fedora)"
 ADDED_REPOS=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
                              | sort | unique[]" /tmp/packages.json))
+REMOVED_REPOS=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+                             | sort | unique[]" /tmp/packages.json))
 INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
                              | sort | unique[]" /tmp/packages.json))
@@ -32,7 +35,7 @@ echo "---"
 
 echo "-- Repositories Management --"
 
-if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+if [[ "${#ADDED_REPOS[@]}" -gt 0 && "${#REMOVED_REPOS[@]}" -eq 0 ]]; then
     wget -P /etc/yum.repos.d/ \
         ${ADDED_REPOS[@]}
 
