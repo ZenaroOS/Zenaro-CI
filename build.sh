0e3f46eeeb403acbfc9df8c10bb730bfb54cf595
SapryWenInera <pedro_americo@id.uff.br>
2023-05-17 18:16:51 -0300

    Added missing fi statement to build.sh

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ echo "-- Repositories Management --"
 if [[ "${#ADDED_REPOS[@]}" -gt 0 && "${#REMOVED_REPOS[@]}" -eq 0 ]]; then
     wget -P /etc/yum.repos.d/ \
         ${ADDED_REPOS[@]}
-
+fi
 echo "---"
 
 echo "Installing RPM-FUSION RPMS"
