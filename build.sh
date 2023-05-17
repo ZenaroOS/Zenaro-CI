ed19b59be90e0f564e957dfde2815f8a081dd050
SapryWenInera <pedro_americo@id.uff.br>
2023-05-17 18:58:20 -0300

    Fix for repos not being downloaded

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ RELEASE="$(rpm -E %fedora)"
 
 ADDED_REPOS=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /tmp/packages.json))
+                             | sort | unique[]" /tmp/repos.json))
 REMOVED_REPOS=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /tmp/packages.json))
+                             | sort | unique[]" /tmp/repos.json))
 INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
                              | sort | unique[]" /tmp/packages.json))
@@ -32,8 +32,8 @@ echo "---"
 echo "-- Repositories Management --"
 
 if [[ "${#ADDED_REPOS[@]}" -gt 0 && "${#REMOVED_REPOS[@]}" -eq 0 ]]; then
-    wget -P /etc/yum.repos.d/ \
-        ${ADDED_REPOS[@]}
+	wget -P /etc/yum.repos.d/ \
+		${ADDED_REPOS[@]}
 fi
 echo "---"
 
