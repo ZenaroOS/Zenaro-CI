50529050d3d2d1cb3628dc3bd487a2a0c38ac684
SapryWenInera <pedro_americo@id.uff.br>
2023-05-25 17:15:14 -0300

    Please Push

--- post-build.sh
+++ post-build.sh
@@ -11,7 +11,3 @@ systemctl enable flatpak-system-update.timer
 systemctl --global enable flatpak-user-update.timer
 
 cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
-
-if [[ -f /usr/lib/appimaged/appimaged-${RELEASE}-x86_64.AppImage ]]; then 
-	wget -P /usr/lib/appimaged/ \
-		https://github.com/probonopd/go-appimage/releases/download/continuous/appimaged-${RELEASE}-x86_64.AppImage
