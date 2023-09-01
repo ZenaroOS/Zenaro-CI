6a876c08db2bef71c9f25f882c812b6f623fe278
ZenaroDev <pedro_americo@id.uff.br>
2023-08-31 23:52:27 -0300

    Rework scripts handling

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != n
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
                              | sort | unique[]" /tmp/packages.json))
 
-SCRIPTS=($(jq -r '.scripts[]' /tmp/scripts.json))
+SCRIPTS=$(jq -r '.scripts[]' /tmp/scripts.json)
 
 echo "-- Running scripts from /tmp/scripts.json"
 echo "${SCRIPTS}"
