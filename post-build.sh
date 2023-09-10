5959bfe2cf756d8d95195747e0273fa2ffed35da
ZenaroDev <pedro_americo@id.uff.br>
2023-09-09 22:46:50 -0300

    Replace Fedora Naming

--- post-build.sh
+++ post-build.sh
@@ -20,7 +20,12 @@ systemctl enable flatpak-system-update.timer
 
 systemctl --global enable flatpak-user-update.timer
 
-if IMAGE_NAME == "sphene" ]]; then
+if [[ IMAGE_NAME == "gahnite" ]]; then
+	sed -i 's/Silverblue/Gahnite/' /usr/lib/os-release
+	sed -i '/^VARIANT_ID/s/silverblue/gahnite/' /usr/lib/os-release
+elif [[ IMAGE_NAME == "sphene" ]]; then
+	sed -i 's/Sericea/Sphene/' /usr/lib/os-release
+	sed -i 	'/^VARIANT_ID/s/sericea/sphene/' /usr/lib/os-release
 	sed -i '171s/kitty-open.desktop;//' /usr/share/applications/mimeinfo.cache
 	sed  -i '12s+inode/directory;++' /usr/share/applications/kitty-open.desktop
 fi
