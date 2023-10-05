ee66000af02dfa4f570a0e47d05e26a4977446a3
ZenaroDev <pedro_americo@id.uff.br>
2023-10-05 12:01:29 -0300

    Fix directory path on setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,5 +5,5 @@ if [[ -f /tmp/setup/zenaro.pub ]]; then
 	cp -f /tmp/setup/policy.json /usr/etc/containers
 else
 	curl  -o /etc/pki/containers/zenaro.pub https://raw.githubusercontent.com/ZenaroDev/container-devel/devel/cosign.pub
-	wget -P /etc/containers/policy.json https://github.com/ZenaroDev/container-devel/raw/devel/setup/policy.json
+	wget -P /etc/containers/ https://github.com/ZenaroDev/container-devel/raw/devel/setup/policy.json
 fi
