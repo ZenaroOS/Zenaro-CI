8f95ba6962923795b70b7b64d7da30dec27832ee
ZenaroDev <pedro_americo@id.uff.br>
2023-10-04 21:04:21 -0300

    Fix extra jump line

--- build/add_rpm.sh
+++ build/add_rpm.sh
@@ -4,7 +4,7 @@ set -oeux pipefail
 echo "-- Adding EXTRA RPMS --"
 wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${FEDORA_MAJOR_VERSION}.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_MAJOR_VERSION}.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_MAJOR_VERSION}.noarch.rpm
 echo "---"
 
 
