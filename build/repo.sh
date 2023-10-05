3b8f7e7cc0da5f91d65ef270af0221acd6f34716
ZenaroDev <pedro_americo@id.uff.br>
2023-10-04 21:14:30 -0300

    Fix variables on repo.sh

--- build/repo.sh
+++ build/repo.sh
@@ -2,12 +2,12 @@
 
 set -ouex pipefail
 
-ADDED_REPOS="$(jq -r '[(.all.include | (.all, select(.\'$IMAGE_NAME\' != null).\'$IMAGE_NAME\')[]), \
-                             (select(.\'$FEDORA_MAJOR_VERSION\' != null).\'$FEDORA_MAJOR_VERSION\'.include | (.all, select(.\'$IMAGE_NAME\' != null).\'$IMAGE_NAME\')[])] \
-                             | sort | unique[]' /tmp/repos.json)"
-REMOVED_REPOS="$(jq -r '[(.all.exclude | (.all, select(.\'$IMAGE_NAME\' != null).\'$IMAGE_NAME\')[]), \
-                             (select(.\'$FEDORA_MAJOR_VERSION\' != null).\'$FEDORA_MAJOR_VERSION\'.exclude | (.all, select(.\'$IMAGE_NAME\' != null).\'$IMAGE_NAME\')[])] \
-                             | sort | unique[]' /tmp/repos.json)"
+ADDED_REPOS=$(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+                             | sort | unique[]" /tmp/repos.json)
+REMOVED_REPOS=$(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+                             | sort | unique[]" /tmp/repos.json)
 
 echo "-- Repositories Management --"
 if [[ "${#ADDED_REPOS[@]}" -gt 0 && "${#REMOVED_REPOS[@]}" -eq 0 ]]; then
