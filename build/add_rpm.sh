9ea648f32ee1c6fb71b9bf5599e68b749bcf2ac9
ZenaroDev <pedro_americo@id.uff.br>
2023-11-03 22:26:03 -0300

    Add RPM Sphere Repo

--- build/add_rpm.sh
+++ build/add_rpm.sh
@@ -4,7 +4,8 @@ set -oeux pipefail
 echo "-- Adding EXTRA RPMS --"
 wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${FEDORA_MAJOR_VERSION}.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_MAJOR_VERSION}.noarch.rpm
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_MAJOR_VERSION}.noarch.rpm \
+    https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-${FEDORA_MAJOR_VERSION}-1.noarch.rpm
 echo "---"
 
 
