bc5266c157be0da958517b2625c7a3e641adcb90
ZenaroDev <pedro_americo@id.uff.br>
2023-11-10 08:53:52 -0300

    Remove RPM sphere from add_rpm.sh

--- build/add_rpm.sh
+++ build/add_rpm.sh
@@ -1,15 +1,8 @@
 #! /bin/sh
 set -oeux pipefail
-UNSTABLE=39
 
-if [[ $FEDORA_MAJOR_VERSION == $UNSTABLE ]]; then
-	FEDORA_MAJOR_VERSION="rawhide"
-fi
 echo "-- Adding EXTRA RPMS --"
 wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${FEDORA_MAJOR_VERSION}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_MAJOR_VERSION}.noarch.rpm \
-    https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-${FEDORA_MAJOR_VERSION}-1.noarch.rpm
 echo "---"
-
-
